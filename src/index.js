f25ea7a789699eacee023e346eb40b450172cc9b
miaroelants <roelantsmia@gmail.com>
2018-10-21 15:35:09 +0200

    store created and functional

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import Store, {doCalculate, addToCalculate, clearAll} from './store'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={Store}><App /></Provider>, document.getElementById('root'));
 
-
+Store.dispatch(addToCalculate('5+4'))
+Store.dispatch(doCalculate())
+Store.dispatch(clearAll())
---
+++ src/store.js
@@ -0,0 +1,56 @@
+import {createStore} from 'redux'
+
+//state
+const initialState = {
+    toCalculate: '',
+    outcome: undefined,
+}
+
+//actions
+export function doCalculate() {
+    return { type: 'DO_CALCULATE', }
+}
+
+export function addToCalculate(input) {
+    return { type: 'ADD_TO_CALCULATE', payload: input}
+}
+
+export function clearAll() {
+    return { type: 'CLEAR_ALL', }
+}
+
+//reducer
+function reducer (state, action) {
+    if (action.type === 'DO_CALCULATE') {
+        const finalToCalculate = eval(state.toCalculate)
+        return {
+            ...state,
+            outcome: finalToCalculate,
+        }
+    }
+    else if (action.type === 'ADD_TO_CALCULATE') {
+        return {
+            ...state,
+            toCalculate: state.toCalculate + action.payload,
+        }
+    }
+    else if (action.type === 'CLEAR_ALL') {
+        return {
+            toCalculate: '',
+            outcome : undefined
+        }
+    }
+    else { return state }
+
+}
+
+//store
+const store = createStore(reducer, initialState)
+
+//(elke keer store wordt aangepast, wordt deze gelogd => testcode)
+store.subscribe(function () {
+    console.log("Updated store, new state:", store.getState())
+})
+
+//export
+export default store
