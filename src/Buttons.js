bec3bb5ae23cd1f283c384827e84a1b0aaafa2c6
miaroelants <roelantsmia@gmail.com>
2018-10-28 00:47:37 +0200

    styled :)

--- src/Buttons.js
+++ src/Buttons.js
@@ -3,10 +3,10 @@ import Button from './button'
 
 // component
 export const Buttons = () => {
-    const inputsNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    const inputsNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     const inputsNumbersButtons = inputsNumbers.map(bleh => <Button data={bleh} id={bleh} />)
     return (
-        <div>
+        <div id="inputnumbers">
             {inputsNumbersButtons}
         </div>
     )
